20dea1f1d2417ce2493ce2be51fe5dfb30f95f97
Tanvi Vikram Gholap <gholaptanvi07@gmail.com>
2026-01-14 01:01:28 +0530

    fix cors

--- functions/index.js
+++ functions/index.js
@@ -1,15 +1,19 @@
+import cors from "cors";
 const express = require("express");
 const cors = require("cors");
-
 const app = express();
-
+app.use(express.json());
 app.use(cors({
   origin: [
     "https://hostel-management-fbproj.web.app",
-  ]
+    "http://localhost:5000"
+  ],
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"]
 }));
 
-app.use(express.json());
+app.options("*", cors());
+
 
 // routes
 app.use("/complaints", require("./routes/complaints"));
